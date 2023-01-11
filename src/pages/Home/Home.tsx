9b5135cfc46498a33260bca13ea5e05fcbb1dc35
fiyintheslim <fiyinoyekunle@gmail.com>
2023-01-11 08:30:37 +0100

    task complete

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Link } from "react-router-dom"
 import { useQuery } from "@tanstack/react-query"
 import { fetchAllPokemon } from '../../network/fetchPokemon'
-import { Spin, Button, Dropdown } from "antd"
+import { Spin, Button, Dropdown, Pagination } from "antd"
 import { ButtonsContainerStyle, ContainerStyle } from "./home.styles"
 import { ErrorMessage } from '../../components/ErrorMessage/ErrorMessage'
 
@@ -35,9 +35,7 @@ export const Home = () => {
                 <h2>Hover here to view Pokemons</h2>
             </Dropdown>
             <ButtonsContainerStyle>
-                <Button disabled={!(!!data?.previous)} onClick={() => page > 0 ? setPage(page - 1) : null}>{"< Previous page"}</Button>
-                <input type="number" value={page} onChange={handleInputChange} />
-                <Button disabled={!(!!data?.next)} onClick={() => setPage(page + 1)}>{"Next page >"}</Button>
+                <Pagination showSizeChanger={false} defaultCurrent={page} total={Math.ceil(data.count / 20)} onChange={(page) => setPage(page)} />
             </ButtonsContainerStyle>
         </ContainerStyle>
     )
